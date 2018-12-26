a0e29955edf51b6f95f6b7450ea17317db204bb1
kaveman909 <david.tracy3@gmail.com>
2018-12-25 20:25:55 -0500

    added graphic of tree for day 8

--- day-8.py
+++ day-8.py
@@ -1,4 +1,5 @@
 from anytree import Node, PostOrderIter
+from anytree.exporter import DotExporter
 i = 0
 node_name = 0
 data = [int(i) for i in open('day-8.txt').readline().split(' ')]
@@ -7,8 +8,7 @@ data = [int(i) for i in open('day-8.txt').readline().split(' ')]
 def get_node(parent_node=None):
     global i
     global node_name
-    node = Node("Node {}".format(node_name),
-                parent=parent_node, meta=list(), value=0)
+    node = Node(str(node_name), parent=parent_node, meta=list(), value=0)
     node_name += 1
     n_children = data[i]
     n_metachars = data[i + 1]
@@ -25,13 +25,19 @@ tree = get_node()
 meta_sum = sum([sum(node.meta) for node in PostOrderIter(tree)])
 print("Part 1: {}".format(meta_sum))
 
+# This works because PostOrderIter iterates starting at the leaves, and
+# works its way towards the root.  Therefore, parents without children will
+# run first, getting a value.  Parents with children are then guaranteed that
+# their children's value will have been computed already.
 for node in PostOrderIter(tree):
     if node.children:
-        for j, child in enumerate(node.children):
-            for k in node.meta:
-                if k == (j + 1):
-                    node.value += child.value
+        for k in node.meta:
+            l = k - 1
+            if (l >= 0) and (l < len(node.children)):
+                node.value += node.children[l].value
     else:
         node.value = sum(node.meta)
 
 print("Part 2: {}".format(tree.root.value))
+DotExporter(tree, nodenamefunc=lambda node: "Node: {}\nValue: {}".format(
+    node.name, node.value)).to_dotfile("tree.dot")
