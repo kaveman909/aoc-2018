b03fce6c1117f306f684117b59f0f1068c6dd58d
kaveman909 <david.tracy3@gmail.com>
2018-12-25 19:06:44 -0500

    finished day 8

--- day-8.py
+++ day-8.py
@@ -1,14 +1,14 @@
-from anytree import Node, RenderTree
+from anytree import Node, PostOrderIter
 i = 0
 node_name = 0
-data = [int(i) for i in open('day-8-test.txt').readline().split(' ')]
+data = [int(i) for i in open('day-8.txt').readline().split(' ')]
 
 
 def get_node(parent_node=None):
     global i
     global node_name
     node = Node("Node {}".format(node_name),
-                parent=parent_node, meta=list())
+                parent=parent_node, meta=list(), value=0)
     node_name += 1
     n_children = data[i]
     n_metachars = data[i + 1]
@@ -22,4 +22,16 @@ def get_node(parent_node=None):
 
 
 tree = get_node()
-print(RenderTree(tree))
+meta_sum = sum([sum(node.meta) for node in PostOrderIter(tree)])
+print("Part 1: {}".format(meta_sum))
+
+for node in PostOrderIter(tree):
+    if node.children:
+        for j, child in enumerate(node.children):
+            for k in node.meta:
+                if k == (j + 1):
+                    node.value += child.value
+    else:
+        node.value = sum(node.meta)
+
+print("Part 2: {}".format(tree.root.value))
