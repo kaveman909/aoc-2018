34af21bc974a9ed4d69132fe5b5674b1fa5e8ceb
David Tracy <david.tracy@ge.com>
2018-12-28 12:13:30 -0500

    cleanup of day 11

--- day-11.py
+++ day-11.py
@@ -1,20 +1,18 @@
 serial_number = 7672
 grid_dim = 300
 g_power_level_sum = 0
-grid = [[0 for _ in range(300)] for _ in range(300)]
+grid = [[0 for _ in range(grid_dim)] for _ in range(grid_dim)]
 
 
 def get_power_level(x, y):
     rack_id = x + 10
-    power_level = rack_id * y
-    power_level += serial_number
-    power_level *= rack_id
-    power_level = int(str(power_level)[-3]) if power_level >= 100 else 0
-    power_level -= 5
+    power_level = (rack_id * y + serial_number) * rack_id
+    power_level = (int(str(power_level)[-3]) if power_level >= 100 else 0) - 5
     return power_level
 
 
-# fill out grid
+# fill out grid; only need to do once
+# (solution is still slow even with pre-computed grid)
 for x in range(1, grid_dim + 1):
     for y in range(1, grid_dim + 1):
         grid[y - 1][x - 1] = get_power_level(x, y)
